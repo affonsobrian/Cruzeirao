499ff0160bb4e8bb7d5f29c8fe3d92d72ceea161
Leonardo Zanone <30538357%2BLeonardoZanone@users.noreply.github.com>
2018-04-04 15:36:28 -0300

    Warning and Error messages in Login

--- src/main/java/sistema/bean/UsuarioManagedBean.java
+++ src/main/java/sistema/bean/UsuarioManagedBean.java
@@ -1,5 +1,6 @@
 package sistema.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -11,6 +12,7 @@ import org.primefaces.event.RowEditEvent;
 import java.io.Console;
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 import java.util.logging.ConsoleHandler;
 
 import sistema.models.Usuario;
@@ -63,23 +65,30 @@ public class UsuarioManagedBean {
 		return event.getNewStep();
 	}
 	
-	public boolean validate() {
+	public boolean validate() throws Exception {
 		
 		usuarios = this.service.getUsuarios();
+		if(usuario.getEmail().isEmpty() || usuario.getSenha().isEmpty()) {
+			if(FacesContext.getCurrentInstance().getViewRoot().getLocale().getLanguage() == "pt")
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Warn", "Usuário ou senha vazio!"));
+			else
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Warn", "User or password is empty!"));
+			return false;
+		}
 		for (Usuario u : usuarios) {
 			if(u.getSenha().equals(usuario.getSenha()))
 			{			
-				try {
-					FacesContext.getCurrentInstance().getExternalContext().redirect("indexLogged.xhtml");
-					System.out.println("Logged in");
-					usuario = u;
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				FacesContext.getCurrentInstance().getExternalContext().redirect("indexLogged.xhtml");
+				System.out.println("Logged in");
+				usuario = u;
 				return true;
 			}
 		}
+		if(FacesContext.getCurrentInstance().getViewRoot().getLocale().getLanguage() == "pt")
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Usuário ou senha incorreto!"));
+		else
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Warn", "User or password is incorrect!"));
+
 		System.out.println("Failed Logging in");
 		return false;
 	}
