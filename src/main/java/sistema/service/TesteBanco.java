732257a2b7d5e6d261cd230171aee743784e78f1
Gustavo <Gustavo@gus>
2018-04-08 19:30:38 -0300

    Alteração do nome do arquivo de logo para banner

--- src/main/java/sistema/service/TesteBanco.java
+++ src/main/java/sistema/service/TesteBanco.java
@@ -1,7 +1,5 @@
 package sistema.service;
 
-
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -13,10 +11,15 @@ public class TesteBanco {
 	public static void main(String[] args) {
 
 		EntityManagerFactory factory;
+		System.out.println(1);
 		EntityManager em;
+		System.out.println(2);
 		factory = Persistence.createEntityManagerFactory("Cruzeirao");
+		System.out.println(3);
 		em = factory.createEntityManager();
+		System.out.println(4);
 		em.find(Grupo.class, 1);
+		System.out.println(5);
 		
 	}
 
