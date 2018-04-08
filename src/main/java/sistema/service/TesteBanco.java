5acac6eec989d24ca62a2490bd72f4a32ebd2230
Gustavo <Gustavo@gus>
2018-04-08 19:53:40 -0300

    Remoção dos comentários feitos para teste

--- src/main/java/sistema/service/TesteBanco.java
+++ src/main/java/sistema/service/TesteBanco.java
@@ -11,16 +11,9 @@ public class TesteBanco {
 	public static void main(String[] args) {
 
 		EntityManagerFactory factory;
-		System.out.println(1);
 		EntityManager em;
-		System.out.println(2);
 		factory = Persistence.createEntityManagerFactory("Cruzeirao");
-		System.out.println(3);
 		em = factory.createEntityManager();
-		System.out.println(4);
-		em.find(Grupo.class, 1);
-		System.out.println(5);
-		
+		em.find(Grupo.class, 1);	
 	}
-
 }
