d05d41632b9b62d18b0f82de887a49443abab536
Leonardo Zanone <30538357%2BLeonardoZanone@users.noreply.github.com>
2018-05-13 20:46:35 -0300

    Language manipulation

--- src/main/java/sistema/bean/locale/LocaleBean.java
+++ src/main/java/sistema/bean/locale/LocaleBean.java
@@ -25,5 +25,37 @@ public class LocaleBean {
         locale = new Locale(language);
         FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
     }
+    
+    public String setPortuguese() {
+    	locale = new Locale("pt");
+    	System.out.println("Setting Pt");
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+        return "./index.xhtml";
+    }
+    
+    public String setEnglish() {
+    	locale = Locale.ENGLISH;
+    	System.out.println("Setting En");
+    	FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    	return "./index.xhtml";
+    }
+    
+    public boolean renderPt() {
+    	System.out.println("Render Pt:");
+    	System.out.println(FacesContext.getCurrentInstance().getViewRoot().getLocale() != Locale.ENGLISH);
+    	if(FacesContext.getCurrentInstance().getViewRoot().getLocale() == Locale.ENGLISH)
+    		return false;
+    	else
+    		return true;
+    }
+    
+    public boolean renderEn() {
+    	System.out.println("Render En:");
+    	System.out.println(FacesContext.getCurrentInstance().getViewRoot().getLocale() == Locale.ENGLISH);
+    	if(FacesContext.getCurrentInstance().getViewRoot().getLocale() == Locale.ENGLISH)
+    		return true;
+    	else
+    		return false;
+    }
 
 }
