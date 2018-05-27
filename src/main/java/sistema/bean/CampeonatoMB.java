ed0c0135314de443219273210314061b9fcca824
Gustavo <Gustavo@gus>
2018-05-27 10:21:21 -0300

    Cadastramento de categoria do campeonato

--- src/main/java/sistema/bean/CampeonatoMB.java
+++ src/main/java/sistema/bean/CampeonatoMB.java
@@ -1,8 +1,5 @@
 package sistema.bean;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -12,9 +9,10 @@ import org.primefaces.event.FlowEvent;
 import org.primefaces.event.RowEditEvent;
 
 import sistema.models.Campeonato;
+import sistema.models.Categoria;
 import sistema.models.Local;
 import sistema.service.CampeonatoService;
-import sistema.service.LocalService;
+
 
 @ManagedBean
 @ViewScoped
@@ -22,9 +20,17 @@ public class CampeonatoMB {
 
 	private Campeonato campeonato = new Campeonato();
 	private Local local = new Local();
+	private Categoria categoria = new Categoria();
 	private CampeonatoService campeonatoService;
-	private LocalService localService;
 
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
 	public Campeonato getCampeonato() {
 		return campeonato;
 	}
@@ -43,22 +49,49 @@ public class CampeonatoMB {
 
 	// Service communication
 	public void salvarLocal() {
-		if (this.getLocal().equals(null))
-			System.out.println("O local é um objeto inválido.");
+		if (this.getLocal().getEndereco() == "") {
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro! O campo de local está inválido.", null);
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
 		else {
 			campeonato.getLocais().add(this.getLocal());
-			FacesMessage msg = new FacesMessage("Local " + this.getLocal().getEndereco() + " cadastrado com sucesso!");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso! O Local " + this.getLocal().getEndereco() + " foi cadastrado", null);
+			FacesContext.getCurrentInstance().addMessage(null, msg);			
 			local = new Local();
 		}
 	}
 
-	public void remover(Local local) {
-		if (this.getLocal().equals(null))
-			System.out.println("O local é um objeto inválido.");
-		else {
-			campeonato.getLocais().remove(this.getLocal());
+	public void removerLocal(Local local) {
+		if(local.getEndereco() != "") {
+			campeonato.getLocais().remove(local);
 		}
+	}
+	
+	public void salvarCategoria() {					
+			System.out.println("Salvar categoria");
+			System.out.println(this.getCategoria().getNome());
+
+			campeonato.getCategorias().add(this.getCategoria());
+			//FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso! A Categoria " + this.getCategoria().getNome() + " foi cadastrada", null);
+			//FacesContext.getCurrentInstance().addMessage(null, msg);
+			categoria = new Categoria();
+		
+	}
+	
+	public void removerCategoria(Categoria categoria) {
+		if(categoria.getNome() != "") {
+			campeonato.getCategorias().remove(categoria);
+		}
+	}
+	
+	public void salvarCampeonato() {
+		System.out.println(this.campeonato.getLocais());
+		System.out.println(this.campeonato.getCategorias());
+		
+		//this.campeonatoService.salvar(this.getCampeonato());
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso! O campeonato foi criado", null);
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		campeonato = new Campeonato();
 	}
 
 	// Wizard manipulation
